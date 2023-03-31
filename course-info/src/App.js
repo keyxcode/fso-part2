6d0a926c270ad36fbf9c1cc669649204bcbb8882
keyxcode <keyxcode@gmail.com>
2023-03-31 12:41:32 +0700

    First commit

---
+++ course-info/src/App.js
@@ -0,0 +1,52 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part name={part.name} exercises={part.exercises} />
+    ))}
+  </div>
+);
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total
+        sum={course.parts.reduce(
+          (prev, current) => prev + current.exercises,
+          0
+        )}
+      />
+    </div>
+  );
+};
+
+export default App;
