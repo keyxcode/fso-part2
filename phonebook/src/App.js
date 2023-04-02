0afa33f9891e0ca9299385c232bbb1a29e9e969e
keyxcode <keyxcode@gmail.com>
2023-04-03 00:11:06 +0700

    Implement input name change for phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -9,12 +9,16 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNewNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
