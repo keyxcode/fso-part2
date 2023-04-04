0259e7a81efb255c3fd450cc87aa3ad418ff5b73
keyxcode <keyxcode@gmail.com>
2023-04-04 13:28:45 +0700

    Solution 2.16: show notification after successful adding operation

--- phonebook/src/components/Notification.js
+++ phonebook/src/components/Notification.js
@@ -1,7 +1,15 @@
 const Notification = ({ msg }) => {
+  const notiStyle = {
+    color: "green",
+    backgroundColor: "lightGrey",
+    border: "5px solid green",
+    borderRadius: 5,
+    padding: 10,
+  };
+
   if (msg === null) return null;
 
-  return <div>{msg}</div>;
+  return <div style={notiStyle}>{msg}</div>;
 };
 
 export default Notification;
